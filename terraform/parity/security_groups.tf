243348ea9f64b54213d1e37a02a83ca6853de498
Fabiano Frizzo <ffrizzo@gmail.com>
2020-01-02 19:15:51 +0000

    Update security group

--- terraform/parity/security_groups.tf
+++ terraform/parity/security_groups.tf
@@ -14,18 +14,8 @@ resource "aws_security_group" "parity" {
   }
 
   ingress {
-    from_port = 8545
-    to_port   = 8545
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
-
-  ingress {
-    from_port = 8546
-    to_port   = 8546
+    from_port = 80
+    to_port   = 80
     protocol  = "tcp"
 
     cidr_blocks = [
